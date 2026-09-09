57e2cd178433b8a40b93241dc7672f69ef423684
zoetshilele61-boop <zoetshilele61@gmail.com>
2026-09-09 04:10:01 +0100

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn z_data:app
---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'z-data-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
